91d08171d123b514f872d4efcd0e27af3c37dc0f
PhucLH <phuclh@rikkeisoft.com>
2025-12-11 14:44:04 +0700

    feat: offline detector

--- App.tsx
+++ App.tsx
@@ -3,6 +3,7 @@ import { HashRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { LanguageProvider } from './contexts/LanguageContext';
 import { OrderProvider } from './contexts/OrderContext';
 import { CustomerProvider } from './contexts/CustomerContext';
+import OfflineDetector from './components/OfflineDetector';
 import Layout from './components/Layout';
 import DashboardPage from './pages/Dashboard/index';
 import OrdersPage from './pages/Orders/index';
@@ -14,26 +15,28 @@ import SePayWebhookViewer from './components/SePayWebhookViewer';
 
 const App: React.FC = () => {
   return (
-    <HashRouter>
-      <LanguageProvider>
-        <OrderProvider>
-          <CustomerProvider>
-            <Routes>
-              <Route path="/" element={<Layout />}>
-                <Route index element={<DashboardPage />} />
-                <Route path="orders" element={<OrdersPage />} />
-                <Route path="transactions" element={<TransactionsPage />} />
-                <Route path="inventory" element={<InventoryPage />} />
-                <Route path="customers" element={<CustomersPage />} />
-                <Route path="settings" element={<SettingsPage />} />
-                <Route path="sepay-webhook" element={<SePayWebhookViewer />} />
-                <Route path="*" element={<Navigate to="/" replace />} />
-              </Route>
-            </Routes>
-          </CustomerProvider>
-        </OrderProvider>
-      </LanguageProvider>
-    </HashRouter>
+    <OfflineDetector>
+      <HashRouter>
+        <LanguageProvider>
+          <OrderProvider>
+            <CustomerProvider>
+              <Routes>
+                <Route path="/" element={<Layout />}>
+                  <Route index element={<DashboardPage />} />
+                  <Route path="orders" element={<OrdersPage />} />
+                  <Route path="transactions" element={<TransactionsPage />} />
+                  <Route path="inventory" element={<InventoryPage />} />
+                  <Route path="customers" element={<CustomersPage />} />
+                  <Route path="settings" element={<SettingsPage />} />
+                  <Route path="sepay-webhook" element={<SePayWebhookViewer />} />
+                  <Route path="*" element={<Navigate to="/" replace />} />
+                </Route>
+              </Routes>
+            </CustomerProvider>
+          </OrderProvider>
+        </LanguageProvider>
+      </HashRouter>
+    </OfflineDetector>
   );
 };
 
